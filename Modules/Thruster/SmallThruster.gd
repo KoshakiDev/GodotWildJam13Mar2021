6d16d98f9ef05bafb6dd4207ebea3cb18297eaac
Mastermori <mastermoriii@gmail.com>
2021-03-15 00:34:40 +0100

    Made thruster thruster-shaped and increased thrust force

--- Modules/Thruster/SmallThruster.gd
+++ Modules/Thruster/SmallThruster.gd
@@ -1,8 +1,8 @@
 extends Module
 
-export var thrust_force := 100
+export var thrust_force := 150
 
 func _unhandled_input(event):
 	if event.is_action_pressed("jump"):
-		print("Thrust in %s" % owner.rotation)
-		owner.apply_impulse($Connections/Connector.position, Vector2.UP.rotated(owner.rotation) * thrust_force)
+		owner.apply_impulse($Connections/Connector.position,
+			Vector2.UP.rotated(owner.rotation) * thrust_force)
