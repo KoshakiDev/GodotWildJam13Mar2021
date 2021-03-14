575975f50775270ffcf4556e0d9ad9b546d0c335
Mastermori <mastermoriii@gmail.com>
2021-03-15 00:33:58 +0100

    Added connection types
    Types are small, medium and large for now.

--- Modules/Connector.gd
+++ Modules/Connector.gd
@@ -2,11 +2,23 @@ class_name Connector
 extends Position2D
 
 
+enum Type {
+	Small = 1,
+	Medium = 2,
+	Big = 4,
+}
+
 # The direction of this connector as a string, to make it easier to export.
 export(String, "Left", "Right", "Up", "Down") var direction_string: String setget set_direction_string
 
+export(Type, FLAGS) var connection_type: int = 2
+
 # The direction of this connector (opposite directions can connect).
 var direction := Vector2.ZERO
+
+# Returns true if this connector can connect to the other connector.
+func can_connect(other_connector: Connector) -> bool:
+	return connection_type & other_connector.connection_type != 0
 
 # Used to set the direction of the connector with export hints.
 func set_direction_string(dir: String):
