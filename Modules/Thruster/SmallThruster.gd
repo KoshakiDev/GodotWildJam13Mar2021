c1c184fb19b7fa91af489618ffd1c69adeb4e47c
Mastermori <mastermoriii@gmail.com>
2021-03-15 16:53:32 +0100

    progress on ModuleUI; added thruster sprite; moved project folders

--- Modules/Thruster/SmallThruster.gd
+++
@@ -1,7 +0,0 @@
-extends Module
-
-export var thrust_force := 150
-
-func use(_event: InputEvent):
-	owner.apply_impulse($Connections/Connector.position,
-		Vector2.UP.rotated(owner.rotation) * thrust_force)
