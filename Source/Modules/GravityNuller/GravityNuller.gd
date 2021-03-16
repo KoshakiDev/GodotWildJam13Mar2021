ff1cab9b64aa9286384301bb70399bb1e0d7c6d6
Eddy Aguirre <eddyj.aguirre@gmail.com>
2021-03-15 23:19:35 -0400

    Anti gravity module

--- Source/Modules/GravityNuller/GravityNuller.gd
+++ Source/Modules/GravityNuller/GravityNuller.gd
@@ -1,5 +1,12 @@
 extends Module
 
+onready var timer: Timer = $Timer
 
 func use(_event: InputEvent):
-	print("null gravity")
+	owner.set_gravity_scale(0.0)
+	timer.start()
+	print("Antigravity")
+
+func _on_Timer_timeout():
+	owner.set_gravity_scale(1.0)
+	print("Gravity back")
