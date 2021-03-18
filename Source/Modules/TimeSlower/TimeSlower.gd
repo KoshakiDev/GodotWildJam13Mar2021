a4492b53c59e08e0c08de86f3478427220556ad5
Mastermori <mastermoriii@gmail.com>
2021-03-18 01:44:23 +0100

    made gravity nuller and time slower toggled modules

--- Source/Modules/TimeSlower/TimeSlower.gd
+++ Source/Modules/TimeSlower/TimeSlower.gd
@@ -1,11 +1,11 @@
 extends Module
 
-onready var timer: Timer = $Timer
+export(float, 0, 1) var time_slow_factor = .5
 
-func use(_event: InputEvent):
-	Engine.set_time_scale(0.5)
-	timer.start()
+var old_time_scale: float
 
-
-func _on_Timer_timeout():
-	Engine.set_time_scale(1)
+func toggle(active: bool):
+	if active:
+		old_time_scale = Engine.time_scale
+	Engine.time_scale = time_slow_factor if active else old_time_scale
+	print("Time slow toggled")
